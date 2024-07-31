e308b20cc17f939ce66d4b6559dd349230e8b800
Aaron Graves <ajgraves@ajgraves.com>
2024-07-31 12:37:11 -0500

    Initial Commit

---
+++ Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by Aaron Graves on 7/31/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
